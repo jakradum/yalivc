a55b1bad39ccc72b6ffa1148b1e843f62566188d
jakradum <145890080+jakradum@users.noreply.github.com>
2024-07-27 14:40:29 +0530

    added extra files for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['plus.unsplash.com'],
   },
+  basePath: '/yalivc',
+  assetPrefix: '/yalivc/',
+  trailingSlash: true,
+  output: 'export',
 };
 
 export default nextConfig;
