aa529819f44a8f6011b59505e78a2105b41dc128
jakradum <145890080+jakradum@users.noreply.github.com>
2024-08-22 13:44:41 +0530

    desktop footer done, moving to mobile

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -11,22 +11,28 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.redSectionFlex}>
         <div className={styles.contentWrapper}>
-            <div className={styles.rowOne}></div>
-          <div className={styles.linksSection}>
-            {navigationItems.map((item, index) => (
-              <div key={index} className={styles.linkBox}>
-                <Link href={item.path}>
-                  {item.name.toUpperCase()} <ArrowLinkOpen />
-                </Link>
-              </div>
-            ))}
-          </div>
-          <div className={styles.infoSection}>
-            <h3>Yali.VC</h3>
-            <p>
-              Yali Capital, headquartered in Bangalore India, is a deep tech fund that invests in early stage companies
-              across a range of sectors.
-            </p>
+          <div className={styles.gridContainer}>
+            <div className={styles.emptyGrid}></div>
+            <div className={styles.emptyGrid}></div>
+            <div className={styles.linksSection}>
+              {navigationItems.map((item, index) => (
+                <div key={index} className={styles.linkBox}>
+                  <Link href={item.path}>
+                    {item.name.toUpperCase()} <ArrowLinkOpen />
+                  </Link>
+                </div>
+              ))}
+            </div>
+            <div className={styles.emptyGridSide}></div>
+            <div className={styles.emptyGridSide}></div>
+            <div className={styles.emptyGridSide}></div>
+            <div className={styles.infoSection}>
+              <h3>Yali.VC</h3>
+              <p>
+                Yali Capital, headquartered in Bangalore, India, invests in early stage deep tech startups across a wide
+                range of verticals from Aerospace to Robotics.
+              </p>
+            </div>
           </div>
         </div>
         <div className={styles.graphicSection}>
@@ -37,7 +43,7 @@ export default function Footer() {
         <Link href="mailto:hello@yali.com">hello@yali.com</Link>
         <Link href="/">LinkedIn</Link>
         <Link href="/">We're hiring - Get in touch</Link>
-        <small>Yali@{new Date().getFullYear()}</small>
+        <small>© Yali Capital {new Date().getFullYear()}</small>
       </div>
     </footer>
   );
