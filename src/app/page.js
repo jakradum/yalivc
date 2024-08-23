357a8985ff942ebf60208a14b539d8676d6f32c1
jakradum <145890080+jakradum@users.noreply.github.com>
2024-08-23 17:29:50 +0530

    landing scroll - desktop
    graphic in place through svg, need to adjust resiszing and viewfinder icons for the h1

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import styles from './styles/page.module.css';
 import Button from './components/button';
 import CompanyShowcase from './components/showcase';
+import { DottedLogoGraphic } from './components/icons/dotted logo graphic';
+import { ViewfinderIcon } from './components/icons/small icons/viewfinder icon';
 
 const companyNames = ['Company1', 'Company2', 'Company3', 'Company4', 'Company5', 'Company6'];
 
@@ -10,51 +12,23 @@ const BASE_LOGO_URL = 'https://yali.vc/wp-content/uploads/2023/10/cosmic-circuit
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <section className={styles.hero}>
-        <div className={styles.heroContent}>
-          <h1 className={styles.title}>Taking deep tech to greater heights, one investment at a time.</h1>
-          <hr />
-          <div className={styles.teamGrid}>
-            <p className={styles.subTitle}>
-              We believe the next wave of large-scale innovation will have roots in deep tech
-            </p>
-            <p className={styles.subTitle}>
-              With investments across sectors like Semiconductors, Robotics, Genomics, Aerospace, and AI, we’re helping
-              India’s startups build for the future, and we’re only getting started.
-            </p>
-          </div>
-        </div>
-      </section>
-
-      <section className={`${styles.section} ${styles.about}`}>
-        <div className={styles.aboutSection}>
-          <h2 className={styles.sectionTitle}>[WE ARE]</h2>
-
-          <div className={styles.aboutBox}>
+    <main>
+      <section className={styles.heroSection}>
+        <aside className={styles.contentWrapper}>
+          <div className={styles.headingBox}>
             <p>
-              Yali Capital is helmed by a team of experts from the world of deep tech with a razor-sharp focus on
-              early-stage companies. We help nurture startups through funding, mentorship, and access to a network of
-              innovators and industry leaders. The core team at Yali Capital includes people from a range of backgrounds
-              such as Semiconductor, Life Sciences, Machine Learning, and Venture Capital.
+              We believe the next wave of large-scale innovation will have roots in deep tech. With investments across
+              sectors like Semiconductors, Robotics, Genomics, Aerospace, and AI, we’re helping India’s startups build
+              for the future, and we’re only getting started.
             </p>
-            <Image src='https://yali.vc/wp-content/uploads/2023/10/cosmic-circuits-1.png' width={160} height={38} alt="Logo" />
+            <ViewfinderIcon/>
+            <h1>Taking Indian deep tech to new heights. One investment at a time.</h1>
+            <ViewfinderIcon />
           </div>
-        </div>
-      </section>
-
-      <section>
-        <CompanyShowcase />
-      </section>
-
-      <section className={styles.team}>
-        <h2 className={styles.sectionTitle}>Meet the team at Yali Capital</h2>
-        <div className={styles.teamGrid}></div>
-      </section>
-
-      <section className={styles.news}>
-        <h2 className={styles.sectionTitle}>Recent news</h2>
-        <div className={styles.newsGrid}></div>
+        </aside>
+        <aside className={styles.graphicWrapper}>
+            <DottedLogoGraphic />
+        </aside>
       </section>
     </main>
   );
