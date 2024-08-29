78baddca26a5861e0a289bdef6758fbc8f537f26
jakradum <145890080+jakradum@users.noreply.github.com>
2024-08-29 15:30:05 +0530

    most of the hover styling done - going to attempt converting it back to divs from table

--- src/app/components/companygrid.js
+++ src/app/components/companygrid.js
@@ -4,6 +4,8 @@ import styles from '../landing page styles/companies.module.css';
 import Button from './button';
 
 const CompanyTable = () => {
+  const dummyImg = 'https://img.freepik.com/free-vector/business-logo_23-2147503133.jpg';
+
   return (
     <div className={styles.companyTableContainer}>
       <div className={styles.sidebar}>
@@ -11,19 +13,23 @@ const CompanyTable = () => {
           Our team's prior investments span a range of startups in the deep tech domain, some of which have made it to
           public markets in India.
         </p>
-        <Button href="/investments" color="black">Know More</Button>
+        <Button href="/investments" color="black">
+          View more
+        </Button>
       </div>
       <table className={styles.companyTable}>
         <tbody>
-          {[0, 1].map(row => (
+          {[0, 1].map((row) => (
             <tr key={row}>
               {companiesData.companies.slice(row * 5, (row + 1) * 5).map((company, index) => (
                 <td key={index} className={styles.companyCell}>
                   <article className={styles.companyContent}>
-                    <div className={styles.companyNumber}><h2>{String(row * 5 + index + 1).padStart(2, '0')}</h2></div>
+                    <div className={styles.companyNumber}>
+                      <h2>{String(row * 5 + index + 1).padStart(2, '0')}</h2>
+                    </div>
                     <h4 className={styles.companyTitle}>{company.name}</h4>
                     <p className={styles.companyCategory}>{company.category}</p>
-                    <div className={styles.imagePlaceholder}></div>
+                    <img src={dummyImg} className={styles.imagePlaceholder}></img>
                     <small className={styles.companyOneLiner}>{company.oneLiner}</small>
                   </article>
                 </td>
