204faed20051eabbdbf0329a8fbfc6374e4b329e
jakradum <145890080+jakradum@users.noreply.github.com>
2024-09-11 16:52:50 +0530

    about page section 1
    both dt and mobile done

--- src/app/about-yali/page.js
+++ src/app/about-yali/page.js
@@ -1,14 +1,30 @@
+import styles from './about styles.module.css';
+import { Nucleus } from '../components/icons/background svgs/nucleus';
+
 export default function AboutYali() {
     return (
-      <div>
-        <h1>About Yali</h1>
-        <p>This is the main About Yali page. Add your content here.</p>
-        
-        <section id="team">
-          <h2>Our Team</h2>
-          <p>Information about the Yali team goes here.</p>
-          {/* Add team member details, photos, etc. */}
-        </section>
-      </div>
+      <section className={styles.mainAbout}>
+        <article className={styles.textContent}>
+          <h1>Powering India's growth from deep within</h1>
+          <div className={styles.paraFlex}>
+          <p>
+            Our conviction in the potential and success of the deep tech domain comes from our first-hand understanding
+            of it. Our experience in founding and operating deep tech companies, along with our experience with previous
+            investments in drone and semiconductor tech tells us we can take bets and take big ones while at it. At Yali
+            Capital, our focus is on IP-led deep tech companies in the early stages of their growth.
+          </p>
+          <p>
+            Yali Capital, headquartered in Bangalore, India, was founded in 2023, with the vision to identify and
+            nurture ideas that are poised to lead the next wave of global innovation, and more so in the Indian context.
+            Across sectors such as semiconductors, genomics, robotics and AI, our investments are chosen not only with
+            their growth in mind but also that of the nation. The team at Yali Capital is armed with an impressive track
+            record of successful investment history, and a combined 60 years in the deep tech domain.
+          </p>
+          </div>
+        </article>
+        <aside className={styles.mainsecGraphic}>
+          <Nucleus/>
+        </aside>
+      </section>
     );
   }
