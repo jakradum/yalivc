0bf0299af2faa238e68048e974c0103b56aeff5c
jakradum <145890080+jakradum@users.noreply.github.com>
2024-09-20 16:25:40 +0530

    contact form added

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,35 @@
+import styles from '../about-yali/about styles.module.css'
+
 export default function Contact() {
-    return (
-      <div>
-        <h1>Contact Us</h1>
-        <p>Contact information and form go here.</p>
-      </div>
-    );
-  }
+  return (
+    <div 
+    className={styles.sectionLevel}
+      style={{
+      width: '100vw',
+      height: '100vh',
+      overflow: 'hidden',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <iframe
+        src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAAdeau5UOEJaUjNKOFU2RVNPRFlYUUhNWkNLR0NKTC4u&embed=true"
+        frameBorder="0"
+        marginWidth="0"
+        marginHeight="0"
+        style={{
+          border: 'none',
+          width: '100%',
+          height: '100%',
+          maxWidth: '100%',
+          maxHeight: '100%',
+        }}
+        allowFullScreen
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        msallowfullscreen="true"
+      >
+      </iframe>
+    </div>
+  );
+}
