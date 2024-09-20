834dc60314691796857c86ac9510a5dafa6202ee
jakradum <145890080+jakradum@users.noreply.github.com>
2024-09-20 15:44:06 +0530

    added footer mandatory

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -38,8 +38,11 @@ export default function Footer() {
             <div className={styles.infoSection}>
               <h3>Yali.VC</h3>
               <p>
-                Yali Capital, headquartered in Bangalore, India, invests in early stage deep tech startups across a wide
-                range of verticals from Aerospace to Robotics.
+                Yali Deeptech Fund I ("Fund"), (a scheme of Yali Ventures)("Trust") is registered with SEBI as a
+                Category II Alternative Investment Fund with effect from 29 January 2024 with the Registration Number
+                IN/AIF2/23-24/1438. The registered office of the Fund is at No. 505, B Block, 3rd Cross, AECS Layout,
+                Kundalahalli, Bengaluru - 560037, Karnataka, India. Yali Partners LLP is the Investment Manager, Amicorp
+                Trustees (India) Private Limited is the Trustee and Yali Sponsors LLP is the Sponsor to the Fund.
               </p>
             </div>
           </div>
