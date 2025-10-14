99302284baa82e57d9703cb37c58a79b543550a7
jakradum <pranavkarnad@gmail.com>
2025-10-14 12:17:17 +0530

    console setup

---
+++ src/app/console/[[...tool]]/page.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './sanity/schemaTypes'
+
+export default defineConfig({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  plugins: [structureTool(), visionTool()],
+  schema: { types: schemaTypes },
+  basePath: '/console'
+})
